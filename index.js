d340fe1474fe6d45ca25d85987f96c4dd3783e19
erion-meta <metaerion79@gmail.com>
2023-09-18 23:47:18 +0200

    Database Setup

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import routes from "./src/routes/apiRoutes";
 
 const app = express();
 const PORT = 3000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:37017/apiDB", {
+  useNewUrlParser: true,
+});
+// .then(() => {
+//   console.log("Connected to MongoDB");
+// })
+// .catch((err) => {
+//   console.error("Error connecting to MongoDB:", err);
+// });
+
+// body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
